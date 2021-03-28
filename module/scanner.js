254c768e914838cfa9f6eb2e4e1f446775ac6600
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2021-03-28 14:21:12 +0300

    Developing some scanning process

---
+++ module/scanner.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const evilScan = require('evilscan');
+const logger = require('./logger');
+
+/**
+ * Scan given host
+ * @param {string} targetIp of host
+ * @param {string} ports in form of 22,80,443
+ * @return {Promise<boolean>}
+ * @constructor
+ */
+exports.ScanIp = async function (targetIp, ports) {
+  const options = {
+    target: targetIp,
+    port: ports,
+    status: 'O', // Default: TROU => Timeout, Refused, Open, Unreachable
+    banner: true
+  };
+  new evilScan(options, async function (error, scan) {
+    scan.on('result', data => {
+      console.log(data);
+    });
+
+    scan.run();
+  });
+  return true;
+};
--- network-scanner.js
+++ network-scanner.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const database = require('./module/database');
+const range = require('ip-range-generator');
+const schedule = require('node-schedule');
+const scanner = require('./module/scanner');
 
+const timeout = ms => new Promise(res => setTimeout(res, ms));
+const scanDelayMs = Number(process.env.SCAN_DELAY_MS);
 
+// Check database existence
+database.DatabaseExists().then(async () => {
+  // Repeat with scheduler
+  // schedule.scheduleJob('10 * * * * *', async () => {
 
-database.DatabaseExists().then(() => {
+  for (let ip of range('192.168.2.1', '192.168.2.254')) {
+    await scanner.ScanIp(ip, '3389');
+    await timeout(scanDelayMs);
+  }
 
-
-
-
+  // });
 });
